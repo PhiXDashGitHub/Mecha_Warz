5d386b40d02b84d32b41a86fd1fb5c8ce7766233
PhiX <PhiXDashGitHub@users.noreply.github.com>
2020-05-28 15:12:55 +0200

    Update

---
+++ Assets/PlayerCharacter.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    //Stats
+    public float maxHealth;
+    private float health;
+    public float maxMagicShield;
+    private float magicShield;
+    public float maxShield;
+    private float shield;
+    public float maxToughness;
+    private float toughness;
+    public float maxEnlightenment;
+    private float enlightenment;
+
+    //Resistance
+    public float fireResistance;
+    public float waterResistance;
+    public float iceResistance;
+    public float electricResistance;
+    public float piercingResistance;
+    public float bludgeoningResistance;
+    public float slashingResistance;
+    public float poisonResistance;
+
+    void Start()
+    {
+        health = maxHealth;
+        magicShield = maxMagicShield;
+        shield = 1;
+
+        StartCoroutine(RegenerateHealth());
+        StartCoroutine(RegenerateMagicShield());
+        StartCoroutine(RegenerateShield());
+    }
+
+    void Update()
+    {
+        //Clamp Values
+        health = Mathf.Clamp(health, 0, maxHealth);
+        magicShield = Mathf.Clamp(magicShield, 0, maxMagicShield);
+        shield = Mathf.Clamp(shield, 0, 1);
+        toughness = Mathf.Clamp(toughness, 0, 25);
+        enlightenment = Mathf.Clamp(enlightenment, 0, 25);
+    }
+
+    IEnumerator RegenerateHealth()
+    {
+        if (health < maxHealth)
+        {
+            health++;
+        }
+
+        yield return new WaitForSeconds(3);
+        StartCoroutine(RegenerateHealth());
+    }
+
+    IEnumerator RegenerateMagicShield()
+    {
+        if (magicShield < maxMagicShield)
+        {
+            magicShield++;
+        }
+
+        yield return new WaitForSeconds(3);
+        StartCoroutine(RegenerateMagicShield());
+    }
+
+    IEnumerator RegenerateShield()
+    {
+        if (shield < 1)
+        {
+            shield = 1;
+        }
+
+        yield return new WaitForSeconds(10);
+        StartCoroutine(RegenerateShield());
+    }
+}
--- Assets/PlayerStats.cs
+++ Assets/PlayerStats.cs
@@ -1,8 +1,41 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-    //Nette Variablen
+    PlayerCharacter character;
+
+    public float level;
+    public float progressionLevel;
+
+    void Awake()
+    {
+        character = GetComponent<PlayerCharacter>();
+    }
+
+    void Update()
+    {
+        //Clamp Values
+        level = Mathf.Clamp(level, 1, 100);
+        progressionLevel = Mathf.Clamp(progressionLevel, 0, 3);
+
+        //Character Stats
+        character.maxHealth = 100 + (level / 2);
+        character.maxMagicShield = 100 + (level / 2);
+        character.maxShield = 4 + (level / 10);
+        character.maxToughness = (level / 4);
+        character.maxEnlightenment = (level / 4);
+
+        //Character Resistance
+        character.fireResistance = progressionLevel / 4;
+        character.waterResistance = progressionLevel / 4;
+        character.iceResistance = progressionLevel / 4;
+        character.electricResistance = progressionLevel / 4;
+        character.piercingResistance = progressionLevel / 4;
+        character.bludgeoningResistance = progressionLevel / 4;
+        character.slashingResistance = progressionLevel / 4;
+        character.poisonResistance = progressionLevel / 4;
+    }
 }
