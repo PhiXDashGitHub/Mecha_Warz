b2790420e56afc85c4ffce64adc8f18cb0595255
PhiX <PhiXDashGitHub@users.noreply.github.com>
2020-06-03 16:11:40 +0200

    Update

---
+++ Assets/Scripts/Game/MapGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapGenerator : MonoBehaviour
+{
+    public Tilemap tileMap;
+    public TileBase testGround;
+
+    public Transform grid;
+    public BoxCollider2D mapBounds;
+
+    void Start()
+    {
+        grid.position = Vector3.right * mapBounds.size.x / 2 + Vector3.up * mapBounds.size.y / 2;
+        tileMap.BoxFill(Vector3Int.CeilToInt(transform.position + Vector3.left * mapBounds.size.x + Vector3.down * mapBounds.size.y), testGround, (int)-mapBounds.size.x, (int)-mapBounds.size.y, (int)mapBounds.size.x, (int)mapBounds.size.y);
+
+        for (int x = 0; x < tileMap.size.x; x++)
+        {
+            for (int y = 0; y < tileMap.size.y; y++)
+            {
+                Vector3Int pos = tileMap.WorldToCell(new Vector3(x, y, 0));
+            }
+        }
+    }
+}
--- Assets/Scripts/Game/PlayerMovement.cs
+++ Assets/Scripts/Game/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public new Rigidbody2D rigidbody2D;
+    public BoxCollider2D bounds;
     public float speed;
 
     Vector3 direction;
@@ -15,6 +16,11 @@ public class PlayerMovement : MonoBehaviour
         float yInput = Input.GetAxisRaw("Vertical");
 
         direction = Vector3.ClampMagnitude(Vector3.right * xInput + Vector3.up * yInput, 1);
+
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x, -bounds.size.x / 2 + 0.5f, bounds.size.x / 2 - 0.5f);
+        pos.y = Mathf.Clamp(pos.y, -bounds.size.y / 2 + 0.5f, bounds.size.y / 2 - 0.5f);
+        transform.position = pos;
     }
 
     void FixedUpdate()
