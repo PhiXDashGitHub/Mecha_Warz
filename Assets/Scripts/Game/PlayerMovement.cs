1434b83ef41b14fae3b3119097e0b46aefbf34d7
PhiX <PhiXDashGitHub@users.noreply.github.com>
2020-06-05 22:40:08 +0200

    Update

--- Assets/Scripts/Game/PlayerMovement.cs
+++ Assets/Scripts/Game/PlayerMovement.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public new Rigidbody rigidbody;
-    public float speed;
+    public float transformSpeed;
+    public float angularSpeed;
+    public float transitionSpeed;
+
+    public Animator animator;
 
     Vector3 direction;
 
@@ -15,10 +19,18 @@ public class PlayerMovement : MonoBehaviour
         float yInput = Input.GetAxisRaw("Vertical");
 
         direction = Vector3.ClampMagnitude(Vector3.right * xInput + Vector3.forward * yInput, 1);
+
+        animator.SetFloat("Move", direction != Vector3.zero ? Mathf.Lerp(animator.GetFloat("Move"), 1, transitionSpeed * Time.deltaTime) : Mathf.Lerp(animator.GetFloat("Move"), 0, transitionSpeed * Time.deltaTime));
     }
 
     void FixedUpdate()
     {
-        rigidbody.MovePosition(transform.position + direction * speed * Time.fixedDeltaTime);
+        if (direction != Vector3.zero)
+        {
+            Quaternion rot = Quaternion.LookRotation(direction, transform.up);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rot, angularSpeed * Time.deltaTime);
+
+            rigidbody.MovePosition(transform.position + transform.forward * transformSpeed * Time.fixedDeltaTime);
+        }
     }
 }
